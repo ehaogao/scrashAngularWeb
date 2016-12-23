ca846b3beed327ae85a102df025462743bc034ab
ehaogao <jerry.s.gao@ericsson.com>
2016-12-23 16:37:32 +0800

    first commit

---
+++ app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroesComponent }      from './images/heroes.component';
+import { HeroDetailComponent }  from './image-manager/hero-detail.component';
+import { HeroFormComponent } from './forms/hero-form.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard',  component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes',     component: HeroesComponent },
+  { path: 'detail/:id/form', component: HeroFormComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
